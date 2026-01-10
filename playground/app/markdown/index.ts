4a0dcce13f9dc4fc0c4399dac1cda38973f547eb
jinghaihan <jhh19980114@gmail.com>
2026-01-10 21:09:42 +0800

    refactor(playground): sort example options

--- playground/app/markdown/index.ts
+++ playground/app/markdown/index.ts
@@ -1,27 +1,29 @@
+import type { SelectOption } from 'vue-stream-markdown'
+
 export const markdownGlob = import.meta.glob('./*.md', { query: '?raw' })
 
 export const DEFAULT_MARKDOWN_PATH = './landing-page.md'
 
 /// keep-sorted
 export const MARKDOWN_NAME: Record<string, string> = {
-  './cjk-support.md': 'CJK Language Support',
-  './code-blocks.md': 'Code Blocks',
-  './footnote.md': 'Footnote',
-  './gfm.md': 'GitHub Flavored Markdown',
-  './html.md': 'Custom Html Render',
-  './image-carousel.md': 'Image Carousel',
-  './landing-page.md': 'Landing Page',
-  './mathematics.md': 'Mathematics',
-  './mermaid.md': 'Mermaid Diagrams',
-  './previewers.md': 'Custom Previewers',
-  './typography.md': 'Typography',
+  'CJK Language Support': './cjk-support.md',
+  'Code Blocks': './code-blocks.md',
+  'Custom Html Render': './html.md',
+  'Custom Previewers': './previewers.md',
+  'Footnote': './footnote.md',
+  'GitHub Flavored Markdown': './gfm.md',
+  'Image Carousel': './image-carousel.md',
+  'Landing Page': './landing-page.md',
+  'Mathematics': './mathematics.md',
+  'Mermaid Diagrams': './mermaid.md',
+  'Typography': './typography.md',
 }
 
-export function getPresetOptions() {
-  const paths = Object.keys(markdownGlob)
-  return paths.map((path) => {
+export function getPresetOptions(): SelectOption[] {
+  return Object.keys(MARKDOWN_NAME).map((name) => {
+    const path = MARKDOWN_NAME[name]!
     return {
-      label: MARKDOWN_NAME[path] || path.split('/').pop()?.split('.').shift() || 'Unknown',
+      label: name || 'Unknown',
       value: path,
     }
   })
