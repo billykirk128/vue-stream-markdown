229b55d27d83ec3a02ec9ca5e782b4b8276ce665
jinghaihan <jhh19980114@gmail.com>
2026-01-13 12:55:03 +0800

    chore: fix docs build

--- docs/.vitepress/theme/components/stream-markdown.vue
+++ docs/.vitepress/theme/components/stream-markdown.vue
@@ -124,10 +124,6 @@ function cleanup() {
         theme: ['kanagawa-lotus', 'kanagawa-dragon'],
       }"
       :code-options="codeOptions"
-      :cdn-options="{
-        baseUrl: 'https://cdn.jsdelivr.net/npm/',
-        mermaid: 'umd',
-      }"
     />
 
     <div class="opacity-0 duration-300 absolute z-10 group-hover:opacity-100 hover:opacity-100 -left-4 -top-4">
